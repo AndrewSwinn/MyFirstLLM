1498764d30dbde5a2a7820d70c89df4ab6252649
ams90 <azswinn@gmail.com>
2025-04-10 15:43:33 +0100

    Glove

--- Bigram.py
+++ Bigram.py
@@ -103,9 +103,9 @@ class FeedForward(nn.Module):
     def __init__(self, embed_size):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(embed_size, embed_size),
+            nn.Linear(embed_size, 4 * embed_size),
             nn.ReLU(),
-            nn.Linear(embed_size, embed_size),
+            nn.Linear(4 * embed_size, embed_size),
             nn.Dropout(dropout)
         )
 
